a3b3496e040fbd24e9f816bc618d341a517e9cb0
Pedro Higor <pedro.higor92@gmail.com>
2026-03-10 15:06:45 -0300

    changing the viewModel/gameModelView to model/GameManager

---
+++ app/src/main/java/com/example/tictacgravity/model/GameManager.kt
@@ -0,0 +1,16 @@
+package com.example.tictacgravity.model
+
+class GameManager {
+
+    var isPlayerTurn: Boolean = true
+
+
+    fun switchTurn(){
+        isPlayerTurn = !isPlayerTurn
+    }
+
+    fun damage(player: Player, damage: Int){
+
+        player.life -= damage
+    }
+}
